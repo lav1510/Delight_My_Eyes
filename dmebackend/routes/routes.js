987e16f55b72163d0d9790ad17d52f303b302840
lav1510 <maria..bugarin@student.upt.ro>
2022-10-22 17:58:55 +0300

    first version for the registration part

--- dmebackend/routes/routes.js
+++ dmebackend/routes/routes.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const signUpTemplateCopy = require('../models/SignUpModels')
+//const bcrypt = require('bcrypt')
 
 router.post('/signup', (request, response)=> {
+
+    //const saltPassword= await bcrypt.genSalt(10)
+    //const securePassword = await bcrypt.hash(request.body.password, saltPassword)
+
     const signedUpUser = new signUpTemplateCopy({
         fullName:request.body.fullName,
         username:request.body.username,
         email:request.body.email,
-        password:request.body.password
+       // password:securePassword
+       password: request.body.password
     })
 
+
     signedUpUser.save()
-    .then(data=>{
+    .then(data =>{
         response.json(data)
     })
     .catch(error =>{

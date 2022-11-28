b33c329becb876de88c3a95e6fdcd6a5e3312224
lav1510 <maria..bugarin@student.upt.ro>
2022-11-28 21:47:11 +0200

    added yt player and heart button

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,19 +1,49 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import data from '../ContextApi'
+import YouTube from 'react-youtube';
+import IconButton from '@mui/material/IconButton';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+
+var getYouTubeID = require('get-youtube-id');
 
 const Home = () => {
   const {userdata,setUserData} = useContext(data)
-  console.log(userdata.firstName)
+  //console.log(userdata.firstName)
+  const [id, setId] = useState()
+  const [active, setActive] = useState(false);
 
+  const handleFav= () => {
+    setActive(!active);
+  };
 
-  const logout = ()=>{
+ const logout = ()=>{
     setUserData({})
   }
+
+
+
+  const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  
+
   return (
-    <div className='container container-home'>
-        <h1>Home page</h1>
-        <h2 className="username-home">Hii 👋 {userdata.firstName} {userdata.lastName}</h2>
+    <div id="cont_home">
+      <div>
+        <YouTube videoId="0zhZtGSOFBw" opts={opts}  />
+      </div>
         <button className='btn' onClick={logout}>Logout</button>
+        <IconButton className='btn' onClick={handleFav} aria-label="favourite" style={{color : active ? "grey" : "red" }}>
+        <FavoriteIcon />
+      </IconButton>
+        <button className='btn' onClick={logout}>Next</button>
     </div>
   )
 }

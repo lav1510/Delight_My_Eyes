ec900d2169a8be8dc6d20c46c85abad12022abcb
lav1510 <maria..bugarin@student.upt.ro>
2022-12-09 17:23:49 +0200

    added localStorage to remeber user after login

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import "./Component/style.css"
 import Home from './Component/Home';
+import AlreadyLogin from './Component/AlreadyLogin';
 import { Login } from './Component/Login';
 import Register from './Component/Register';
-import "./Component/style.css"
+import MyFavourites from './Component/MyFavourites';
+import AdminPage from './Component/AdminPage';
+import Navbar from "./Component/Navbar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import data from './ContextApi';
-import { useState } from 'react';
-import Navbar from "./Component/Navbar";
+import { useState, useEffect } from 'react';
 
 function App() {
   const [userdata,setUserData] = useState({})
-  
-  return (
-    
+  const [loggedInUser, setLoggedInUser] = useState({});
+  useEffect(() => {
+    setLoggedInUser( localStorage.getItem("userr"));
+  }, []);
+
+  return ( 
     <div className="App">
       <Navbar />
       <data.Provider value={{userdata,setUserData}}>
         <Router>
           <Routes>
-            <Route path="/"
-              element={userdata && userdata._id ? <Home /> : <Login/>}
-            />
-            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={loggedInUser? <AlreadyLogin /> : <Login/>}/>
             <Route path="/register" element={<Register />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/myfavourites" element={<MyFavourites />} />
           </Routes>
         </Router>
       </data.Provider>
-
     </div>
+    
   );
 }
 
